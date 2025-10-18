940264337c61bd0c8200d866acecefea3ce43425
Salomon_PR <starwoker1@gmail.com>
2025-10-18 01:08:54 -0500

    Feat: Primera configuracion del proyecto BackEnd con la entidad de Persona.

---
+++ DevsuBackEnd/DevsuBackEnd.Infra/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,48 @@
+﻿using DevsuBackEnd.Infra.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevsuBackEnd.Infra.Persistence.Context;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Persona> Personas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=USUARIO\\MSSQLSERVER19;Database=DevsuApplicationBD;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Persona>(entity =>
+        {
+            entity.HasKey(e => e.PersonaId).HasName("PK__Persona__7C34D3034212BF2B");
+
+            entity.ToTable("Persona");
+
+            entity.Property(e => e.Direccion).HasMaxLength(200);
+            entity.Property(e => e.Estado)
+                .IsRequired()
+                .HasDefaultValueSql("((1))");
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.FechaModificacion).HasColumnType("datetime");
+            entity.Property(e => e.Identificacion).HasMaxLength(50);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DevsuBackEnd/DevsuBackEnd.Infra/Persistence/Entities/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevsuBackEnd.Infra.Persistence.Entities;
+
+public partial class Persona
+{
+    public int PersonaId { get; set; }
+    public string Nombre { get; set; } = null!;
+    public bool? Genero { get; set; }
+    public short? Edad { get; set; }
+    public string Identificacion { get; set; } = null!;
+    public string? Direccion { get; set; }
+    public string? Telefono { get; set; }
+    public DateTime FechaCreacion { get; set; }
+    public DateTime? FechaModificacion { get; set; }
+    public bool? Estado { get; set; }
+}
